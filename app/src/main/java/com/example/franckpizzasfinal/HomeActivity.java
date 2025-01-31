a4fd102c2fcc1dcb11d24ba6f3e35119ccaf7ff0
cevos55 <135757442+cevos55@users.noreply.github.com>
2025-01-31 19:52:20 +0100

    Update HomeActivity.java

--- app/src/main/java/com/example/franckpizzasfinal/HomeActivity.java
+++ app/src/main/java/com/example/franckpizzasfinal/HomeActivity.java
@@ -2,12 +2,16 @@ package com.example.franckpizzasfinal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
@@ -25,23 +29,46 @@ public class HomeActivity extends AppCompatActivity {
                     return true;
                 } else if (itemId == R.id.nav_menu) {
                     startActivity(new Intent(HomeActivity.this, MenuActivity.class));
-                    overridePendingTransition(0, 0);
-                    return true;
                 } else if (itemId == R.id.nav_orders) {
                     startActivity(new Intent(HomeActivity.this, OrdersActivity.class));
-                    overridePendingTransition(0, 0);
-                    return true;
                 } else if (itemId == R.id.nav_cart) {
                     startActivity(new Intent(HomeActivity.this, CartActivity.class));
-                    overridePendingTransition(0, 0);
-                    return true;
                 } else if (itemId == R.id.nav_account) {
                     startActivity(new Intent(HomeActivity.this, AccountActivity.class));
-                    overridePendingTransition(0, 0);
-                    return true;
                 }
-                return false;
+                overridePendingTransition(0, 0);
+                return true;
             }
         });
+
+        Button btnMargherita = findViewById(R.id.button_margherita);
+        if (btnMargherita != null) {
+            btnMargherita.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "De retour mon cher ami", Toast.LENGTH_LONG).show();
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_margherita) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Hello");
+            builder.setIcon(R.drawable.piz_log);
+            builder.setMessage("Hello, cette fonctionnalite est en cours de devellopement");
+            builder.setPositiveButton("OK", null);
+            builder.show();
+        }
     }
 }
